788f5c578f9aa778999573a5aaea9a2e5b6d8851
lc-masa <myamanakalife6911@gmail.com>
2023-03-31 17:59:45 +0900

    updated

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
+import { Grid } from "@mui/material";
+import { Container } from "@mui/system";
 import Navbar from "./components/Navbar";
+import Result from "./components/Result";
+import SliderSelect from "./components/SliderSelect";
+import TenureSelect from "./components/TenureSelect";
 
 function App() {
   return (
