8035109e5e7f9319c22d10e3eb9e91d45eb8297e
lc-masa <myamanakalife6911@gmail.com>
2023-03-31 17:51:41 +0900

    updated

--- src/index.js
+++ src/index.js
@@ -4,11 +4,20 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import { theme } from './theme';
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+
+  <ThemeProvider theme={theme}>
+    <React.StrictMode>
+      <App />
+      <CssBaseline />
+    </React.StrictMode>
+  </ThemeProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
