2b5157202fa124455bc91e02cd0b25daac040080
lc-masa <myamanakalife6911@gmail.com>
2023-04-11 11:41:44 +0900

    updated results.js

--- src/components/Result.js
+++ src/components/Result.js
@@ -1,6 +1,10 @@
 import { Stack, Typography } from "@mui/material";
-import React from "react";
+import React from 'react';
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
 import { Pie } from 'react-chartjs-2';
+
+ChartJS.register(ArcElement, Tooltip, Legend);
+
 
 const Result = ({ data }) => {
   // destructure the data
@@ -15,9 +19,34 @@ const Result = ({ data }) => {
 
   const totalInterestGenerated = monthlyPayment * totalLoanMonths - loanAmount;
 
+
+  const pieChartData = {
+    labels: ['Principle', 'Interest'],
+    datasets: [
+      {
+        label: 'Ratio of Principle and Interest',
+        data: [homeValue, totalInterestGenerated],
+        backgroundColor: [
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 159, 64, 0.2)',
+        ],
+        borderColor: [
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 159, 64, 1)',
+        ],
+        borderWidth: 1,
+      },
+    ],
+  };
+
   return (
-    <Stack>
+    <Stack gap={3}>
       <Typography textAlign='center' variant='h5'>Monthly Payment: $ {monthlyPayment.toFixed(2)}</Typography>
+      <Stack direction='row' justifyContent='center'>
+        <div>
+          <Pie data={pieChartData} />
+        </div>
+      </Stack>
     </Stack>
   );
 };
